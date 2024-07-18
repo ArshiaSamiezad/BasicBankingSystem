2526972fdcd5c3f112d3db65917fc3181d5906c7
Arshia Samiezad <45089685+ArshiaSamiezad@users.noreply.github.com>
2024-07-18 13:47:24 +0330

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class Account {
+    private static ArrayList<Account> allAccounts = new ArrayList<Account>();
+    private Bank bank;
+    private int id;
+    private int money;
+    private int remainingDuration;
+    private int interest;
+    private Customer customer;
+
+    public Account(Bank bank, int id, int money, int remainingDuration, int interest, Customer customer) {
+        this.bank = bank;
+        this.id = id;
+        this.money = money;
+        this.remainingDuration = remainingDuration;
+        this.interest = interest;
+        this.customer = customer;
+        allAccounts.add(this);
+    }
+
+    public static void passMonth() {
+        for (int i = 0; i < allAccounts.size(); i++) {
+            Account account = allAccounts.get(i);
+            allAccounts.get(i).passMonthEach();
+            if(i<allAccounts.size()){
+                if(!(account.equals(allAccounts.get(i)))){
+                    i--;
+                }
+            }
+        }
+    }
+
+    public static void deleteAccount(Account account) {
+        for (int i = 0; i < allAccounts.size(); i++) {
+            if (allAccounts.get(i).equals(account)) {
+                allAccounts.remove(i);
+            }
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getAmountOfMoneyForLeaving() { // It will be included in passMonth or passEachMonth
+        if(remainingDuration == 0){
+            return money * (1.0+(Double.valueOf(interest))/100.0);
+        }
+        return money;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    private void passMonthEach() {
+        remainingDuration--;
+        if (remainingDuration == 0) {
+            customer.leaveAccount(this.getId());
+        }
+    }
+}
